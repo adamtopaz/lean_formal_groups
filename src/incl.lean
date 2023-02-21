680b53ed2365fa9ee63db0d09447828cf156641b
adamtopaz <github@adamtopaz.com>
2023-02-21 10:50:39 -0700

    small changes

--- src/incl.lean
+++ src/incl.lean
@@ -22,13 +22,15 @@ end
 #check @dif_pos
 #check tactic.split_ifs
 
+variables (α β R)
+
 def incl_fun : mv_power_series α R → mv_power_series (α ⊕ β) R := 
   show ((α →₀ ℕ) → R) → ((α ⊕ β →₀ ℕ) → R), from λ f m, 
   let n := finsupp.sum_finsupp_equiv_prod_finsupp m in
   if n.2 = 0 then f n.1 else 0
 
 def incl : mv_power_series α R →+* mv_power_series (α ⊕ β) R :=
-{ to_fun := incl_fun,
+{ to_fun := incl_fun α β R,
   map_one' := sorry,
   map_mul' := sorry,
   map_zero' := sorry,
@@ -40,7 +42,7 @@ def incr_fun : mv_power_series β R → mv_power_series (α ⊕ β) R :=
   if n.1 = 0 then f n.2 else 0
 
 def incr : mv_power_series β R →+* mv_power_series (α ⊕ β) R :=
-{ to_fun := incr_fun,
+{ to_fun := incr_fun α β R,
   map_one' := sorry,
   map_mul' := sorry,
   map_zero' := sorry,
