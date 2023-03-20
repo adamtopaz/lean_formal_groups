a7a8c911b249712d82122a9b0786e55ce2a041a0
colejagdtiger <coleton.kotch@gmail.com>
2023-03-20 15:32:33 -0600

    added inclusion functions

--- src/basic.lean
+++ src/basic.lean
@@ -1,7 +1,12 @@
+import tactic
 import ring_theory.ideal.quotient
 import ring_theory.power_series.basic
+import ring_theory.polynomial.basic
 
 noncomputable theory
+open_locale classical
+open_locale big_operators
+
 
 variables (σ τ ω R : Type*) [comm_ring R]
 
@@ -18,11 +23,71 @@ variables {σ τ ω R}
 def congruent_mod (n : ℕ) (F G : mv_power_series σ R) : Prop :=
 F - G ∈ degree_geq σ R n
 
+
+open finset
+
+variables {σ τ ω R}
+def nth_pow (n : ℕ) (F : mv_power_series σ R) : mv_power_series σ R := (∏ i in range n, F)
+
+
+def incl_fun : mv_power_series σ R → mv_power_series (σ ⊕ τ) R := 
+  show ((σ →₀ ℕ) → R) → ((σ ⊕ τ →₀ ℕ) → R), from λ f m, 
+  let n := finsupp.sum_finsupp_equiv_prod_finsupp m in
+  if n.2 = 0 then f n.1 else 0
+
+#check mv_polynomial.eval₂_hom 
+-- mv_polynomial.C or.inl
+/--begin
+  ext m,
+  unfold incl_fun,
+  rw coeff_one,
+  split_ifs,
+  end,-/
+def incl : mv_power_series σ R →+* mv_power_series (σ ⊕ τ) R :=
+{ to_fun := incl_fun,
+  map_one' :=  begin
+  ext n,
+  unfold incl_fun,
+  rw coeff_one,
+  simp,
+  split_ifs,
+  end,
+  map_mul' := begin
+  intros F G,
+  ext,
+  unfold incl_fun,
+  simp,
+  rw coeff_mul,
+  end,
+  map_zero' := begin
+  ext,
+  unfold incl_fun,
+  simp,
+  refl,
+  end,
+  map_add' := sorry }
+
+def incr_fun : mv_power_series τ R → mv_power_series (σ ⊕ τ) R := 
+  show ((τ →₀ ℕ) → R) → ((σ ⊕ τ →₀ ℕ) → R), from λ f m, 
+  let n := finsupp.sum_finsupp_equiv_prod_finsupp m in
+  if n.1 = 0 then f n.2 else 0
+
+def incr : mv_power_series τ R →+* mv_power_series (σ ⊕ τ) R :=
+{ to_fun := incr_fun,
+  map_one' := sorry,
+  map_mul' := sorry,
+  map_zero' := sorry,
+  map_add' := sorry }
+
+
+def change_var (e : σ ≃ τ) : mv_power_series σ R ≃+* mv_power_series τ R := 
+sorry
+
 def compose 
   (F : mv_power_series σ R) -- F(X_1,X_2,...,X_n) 
   (G : σ → mv_power_series τ R) -- G_1(X_1,...,X_m), ..., G_n(X_1,...,X_m)
   (hG : ∀ i, congruent_mod 1 (G i) 0) :
-  mv_power_series τ R := sorry -- F(G_1(X_1,...,X_m),...,G_n(X_1,...,X_m))
+  mv_power_series τ R := λ (n : τ →₀ ℕ ), --(n : (τ →₀ ℕ) => ) -- F(G_1(X_1,...,X_m),...,G_n(X_1,...,X_m))
 
 def compose_fst 
   (F : mv_power_series (σ ⊕ τ) R) -- F(X_1,...,X_n;Y_1,...,Y_m)
@@ -38,8 +103,6 @@ def compose_snd
   mv_power_series (σ ⊕ ω) R := -- F(X_1,...,X_n;G_1(Z_1,...,Z_k),...,G_m(Z_1,...,Z_k))
 sorry 
 
-def change_var (e : σ ≃ τ) : mv_power_series σ R ≃+* mv_power_series τ R := 
-sorry
 
 end mv_power_series
 
